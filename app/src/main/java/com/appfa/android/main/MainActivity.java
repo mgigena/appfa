0d50db9dc7634f95dff47055604ac9327f311a9a
mauricio gigena <mgigena@MacBook-Pro-9.local>
2018-08-09 11:44:15 -0300

    Fix BackPressed
    - Se soluciona bug al presionar back button el cual no permitia salir de
    la app

--- app/src/main/java/com/appfa/android/main/MainActivity.java
+++ app/src/main/java/com/appfa/android/main/MainActivity.java
@@ -1,47 +1,31 @@
 package com.appfa.android.main;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v4.view.GravityCompat;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
-import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 
 import com.appfa.android.R;
 import com.appfa.android.base.AppFaBaseActivity;
 import com.appfa.android.model.dto.TeamDTO;
 import com.appfa.android.team.TeamFragment;
 
-import butterknife.BindView;
-
 public class MainActivity extends AppFaBaseActivity<MainView, MainPresenter> implements MainView,
         NavigationView.OnNavigationItemSelectedListener,
-        TeamFragment.OnListFragmentInteractionListener {
+        TeamFragment.OnListFragmentInteractionListener, MainViewDelegate.MainDelegateCallback {
 
-    @BindView(R.id.drawer_layout)
-    DrawerLayout drawerLayout;
-    @BindView(R.id.addButton)
-    FloatingActionButton floatingActionButton;
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
-    @BindView(R.id.navigation)
-    NavigationView navView;
+    private MainViewDelegate delegate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setContentView(R.layout.activity_main);
         super.onCreate(savedInstanceState);
 
-        configureToolbar();
-        configureNavigationDrawer();
-        setAddButtonConfig();
+        delegate = new MainViewDelegate(getWindow().getDecorView().getRootView(), this);
     }
 
     @Override
@@ -55,39 +39,17 @@ public class MainActivity extends AppFaBaseActivity<MainView, MainPresenter> imp
         return true;
     }
 
-    private void configureToolbar() {
-        setSupportActionBar(toolbar);
-        ActionBar actionbar = getSupportActionBar();
-        actionbar.setHomeAsUpIndicator(R.mipmap.ic_burger);
-        actionbar.setDisplayHomeAsUpEnabled(true);
-    }
-
-    private void configureNavigationDrawer() {
-        navView.getMenu().getItem(0).setChecked(true);
-        onNavigationItemSelected(navView.getMenu().getItem(0));
-        navView.setNavigationItemSelectedListener(this);
-    }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         int itemId = item.getItemId();
         switch (itemId) {
             // Android home
             case android.R.id.home:
-                drawerLayout.openDrawer(GravityCompat.START);
+                delegate.openDrawer();
                 return true;
             // manage other entries if you have it ...
         }
         return true;
-    }
-
-    private void setAddButtonConfig() {
-        floatingActionButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // TODO tener en cuenta la seleccion del drawer
-            }
-        });
     }
 
     @Override
@@ -108,7 +70,9 @@ public class MainActivity extends AppFaBaseActivity<MainView, MainPresenter> imp
             FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
             transaction.replace(R.id.frame_container, fragment);
             transaction.commit();
-            drawerLayout.closeDrawers();
+            if(delegate != null) {
+                delegate.closeDrawer();
+            }
             return true;
         }
         return false;
@@ -122,8 +86,15 @@ public class MainActivity extends AppFaBaseActivity<MainView, MainPresenter> imp
 
     @Override
     public void onBackPressed() {
-        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
-            drawerLayout.closeDrawers();
+        if (delegate.isDrawerOpen()) {
+            delegate.closeDrawer();
+        } else {
+            super.onBackPressed();
         }
+    }
+
+    @Override
+    public Activity getActivity() {
+        return this;
     }
 }
